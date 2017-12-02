681ddb70c01acc73a0874bb3f21c6d01dc64da81
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-12-02 15:47:56 +0100

    Updated source (cleanup)

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+#!/usr/bin/env node
+
+/* StromDAO Business Object: Quittance
+ * =========================================
+ * Accounting related helpers and integrations
+ * 
+ * @author Thorsten Zoerner thorsten.zoerner@stromdao.com 
+ * 
+ * If used in StromDAO-BO's MAIN BRANCH this will be defaulted to the testnet environment.
+ * 
+ */
+ 
 const vorpal = require('vorpal')();
 
 var cli = new require("stromdao-cli-helper")(vorpal);
